24c5d39025b92f9e00cc44b81a6a60fb0c831826
Eds <steadyede@live.co.uk>
2021-02-26 19:10:57 +0000

    Setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,25 @@
+# Se the environment to "test"
+ENV['RACK_ENV'] = 'test'
+
+# Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+# Require all the testing gems
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'features/web_helpers'
+
+# Tell Capybara to talk to the program
+Capybara.app = Chitter
+
 require 'simplecov'
 require 'simplecov-console'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
